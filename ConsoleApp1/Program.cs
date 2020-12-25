a86aa81d603ed46e7ad5a806e7106f4a84d83f5f
eswokarate <46426415+eswokarate@users.noreply.github.com>
2020-12-25 16:09:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первое знакомство с GitHub");
+        }
+    }
+}
