30c92a45c1b4ea4ec98bfe9babc6f9859d17262c
eswokarate <46426415+eswokarate@users.noreply.github.com>
2020-12-25 16:27:56 +0300

    Добавление строки

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Проверка изменений на сервере.");
             Console.WriteLine("Первое знакомство с GitHub");
         }
     }
